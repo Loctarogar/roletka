9662fb048c0750c9018a98975c32575e75ab9ef6
Loctarogar <igrsl1990@gmail.com>
2021-09-05 19:37:28 +0200

    carousel btn logick added

--- scripts/style.js
+++ scripts/style.js
@@ -3,37 +3,42 @@ const carouselBtnNext = document.getElementById("btn_carousel_next");
 const carouselImages = document.querySelectorAll(".carousel img");
 
 let currentSlider = 0;
-console.log(currentSlider);
-console.log(carouselImages.length);
 
 carouselBtnNext.addEventListener("click", nextSlide);
 carouselBtnPrev.addEventListener("click", prevSlide);
 
 function nextSlide() {
-  carouselImages[currentSlider].classList.toggle("current");
-  carouselImages[currentSlider].classList.toggle("non-displayable");
+  classToggle();
   if (carouselImages[currentSlider + 1]) {
-    console.log(carouselImages[currentSlider + 1]);
     currentSlider = currentSlider + 1;
-    console.log(currentSlider);
-    carouselImages[currentSlider].classList.toggle("current");
-    carouselImages[currentSlider].classList.toggle("non-displayable");
+    classToggle();
   }
 
-  if (currentSlider + 1 == carouselImages.length) {
-    carouselBtnNext.classList.toggle("non-displayable");
-  }
+  carouselBtnVisibility();
 }
 
 function prevSlide() {
-  carouselImages[currentSlider].classList.toggle("current");
-  carouselImages[currentSlider].classList.toggle("non-displayable");
+  classToggle();
   currentSlider = currentSlider - 1;
+  classToggle();
+  carouselBtnVisibility();
+}
+
+function classToggle() {
   carouselImages[currentSlider].classList.toggle("current");
   carouselImages[currentSlider].classList.toggle("non-displayable");
-  console.log(currentSlider);
+}
 
-  if (currentSlider == 0) {
-    carouselBtnNext.classList.toggle("non-displayable");
+function carouselBtnVisibility() {
+  console.log(currentSlider);
+  if (currentSlider <= 0) {
+    carouselBtnPrev.classList.add("non-displayable");
+  } else {
+    carouselBtnPrev.classList.remove("non-displayable");
+  }
+  if (currentSlider + 1 == carouselImages.length) {
+    carouselBtnNext.classList.add("non-displayable");
+  } else {
+    carouselBtnNext.classList.remove("non-displayable");
   }
 }
