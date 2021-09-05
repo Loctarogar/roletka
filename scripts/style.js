e64321b4311305fda3737270057d91fe151f3f56
Loctarogar <igrsl1990@gmail.com>
2021-09-05 19:13:53 +0200

    working on carousel

--- scripts/style.js
+++ scripts/style.js
@@ -1,1 +1,33 @@
-console.log("1");
+const carouselBtnPrev = document.getElementById("btn_carousel_prev");
+const carouselBtnNext = document.getElementById("btn_carousel_next");
+const carouselImages = document.querySelectorAll(".carousel img");
+
+let currentSlider = 0;
+console.log(currentSlider);
+console.log(carouselImages[0]);
+
+carouselBtnNext.addEventListener("click", nextSlide);
+carouselBtnPrev.addEventListener("click", prevSlide);
+
+function nextSlide() {
+  carouselImages[currentSlider].classList.toggle("current");
+  carouselImages[currentSlider].classList.toggle("non-displayable");
+  if (carouselImages[currentSlider + 1]) {
+    currentSlider = currentSlider + 1;
+    carouselImages[currentSlider].classList.toggle("current");
+    carouselImages[currentSlider].classList.toggle("non-displayable");
+  } else {
+    carouselBtnNext.classList.toggle("non-displayable");
+  }
+
+  console.log(currentSlider);
+}
+
+function prevSlide() {
+  carouselImages[currentSlider].classList.toggle("current");
+  carouselImages[currentSlider].classList.toggle("non-displayable");
+  currentSlider = currentSlider - 1;
+  carouselImages[currentSlider].classList.toggle("current");
+  carouselImages[currentSlider].classList.toggle("non-displayable");
+  console.log(currentSlider);
+}
