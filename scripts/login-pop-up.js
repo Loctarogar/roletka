b1507f325c808887f5dd0142be416963ac14d18a
Loctarogar <igrsl1990@gmail.com>
2021-10-06 21:34:29 +0200

    login pop-up.js

--- scripts/login-pop-up.js
+++ scripts/login-pop-up.js
@@ -1,1 +1,17 @@
-console.log("a");
+const userLoginIcon = document.querySelector(".user.header-icon-container");
+const userLogin = document.querySelector(".user-login");
+
+userLoginIcon.addEventListener("click", showMenu);
+
+// functions
+function showMenu() {
+  userLogin.classList.toggle("non-displayable");
+  underlay_for_ham_menu.classList.toggle("non-displayable");
+  body.style.overflow = "hidden";
+}
+
+function hideMenu() {
+  userLogin.classList.toggle("non-displayable");
+  underlay_for_ham_menu.classList.toggle("non-displayable");
+  body.style.overflow = "visible";
+}
