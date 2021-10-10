de2bf5135dd92116892484861fb74c7032f5401e
Loctarogar <igrsl1990@gmail.com>
2021-10-10 12:31:57 +0200

    cart carousel works

--- scripts/cart.js
+++ scripts/cart.js
@@ -27,42 +27,71 @@ const currentCartSubContainer = document.querySelector(
 btnCartCarouselPrev.addEventListener("click", prevCartCarouselSubContainer);
 btnCartCarouselNext.addEventListener("click", nextCartCarouselSubContainer);
 
-function nextCartCarouselSubContainer() {
-  let currentCartSubContainer = document.querySelector(
-    ".viewed-sub-container.current-cart-sub-container"
-  );
+changeButtonVisibility(0);
 
+function nextCartCarouselSubContainer() {
+  let currentCartSubContainer = getCurrentCartSubContainer();
+
+  console.log("in next");
   for (i = 0; i < allCartCarouselSubContainers.length; i++) {
     if (allCartCarouselSubContainers[i] == currentCartSubContainer) {
       if (i + 1 < allCartCarouselSubContainers.length) {
         let nextCartSubContainer = allCartCarouselSubContainers[i + 1];
-        currentCartSubContainer.classList.remove("current-cart-sub-container");
-        currentCartSubContainer.classList.add("non-displayable");
-        nextCartSubContainer.classList.remove("non-displayable");
-        nextCartSubContainer.classList.add("current-cart-sub-container");
-      } else {
-        btnCartCarouselNext.classList.add("non-displayable");
+        changeCartSubContainerClasses(
+          currentCartSubContainer,
+          nextCartSubContainer
+        );
       }
+      changeButtonVisibility(i + 1);
     }
   }
 }
 
 function prevCartCarouselSubContainer() {
-  let currentCartSubContainer = document.querySelector(
-    ".viewed-sub-container.current-cart-sub-container"
-  );
+  console.log("in prev");
+  let currentCartSubContainer = getCurrentCartSubContainer();
 
   for (i = allCartCarouselSubContainers.length - 1; i >= 0; i--) {
     if (allCartCarouselSubContainers[i] == currentCartSubContainer) {
       if (i - 1 >= 0) {
         let nextCartSubContainer = allCartCarouselSubContainers[i - 1];
-        currentCartSubContainer.classList.remove("current-cart-sub-container");
-        currentCartSubContainer.classList.add("non-displayable");
-        nextCartSubContainer.classList.remove("non-displayable");
-        nextCartSubContainer.classList.add("current-cart-sub-container");
-      } else {
-        btnCartCarouselPrev.classList.add("non-displayable");
+        changeCartSubContainerClasses(
+          currentCartSubContainer,
+          nextCartSubContainer
+        );
       }
+      changeButtonVisibility(i - 1);
     }
+  }
+}
+
+function getCurrentCartSubContainer() {
+  return document.querySelector(
+    ".viewed-sub-container.current-cart-sub-container"
+  );
+}
+
+function changeCartSubContainerClasses(
+  currentCartSubContainer,
+  nextCartSubContainer
+) {
+  currentCartSubContainer.classList.remove("current-cart-sub-container");
+  currentCartSubContainer.classList.add("non-displayable");
+  nextCartSubContainer.classList.remove("non-displayable");
+  nextCartSubContainer.classList.add("current-cart-sub-container");
+}
+
+function changeButtonVisibility(i) {
+  console.log("in change buton - " + i);
+  if (i >= allCartCarouselSubContainers.length - 1) {
+    btnCartCarouselNext.classList.add("non-displayable");
+  } else {
+    btnCartCarouselNext.classList.remove("non-displayable");
+  }
+
+  if (i <= 0) {
+    btnCartCarouselPrev.classList.add("non-displayable");
+  } else {
+    btnCartCarouselPrev.classList.remove("non-displayable");
   }
 }
